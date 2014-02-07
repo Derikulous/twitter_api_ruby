69fb96a8ee05043e014210af069e301aa63f330a
Derik Strattan <derik.strattan@gmail.com>
2014-02-06 20:34:48 -0800

    looking at chatterbot

--- userstream.rb
+++ userstream.rb
@@ -1,9 +1,13 @@
-require 'tweetstream'
+require 'chatterbot/dsl'
 
 TweetStream.configure do |config|
-  config.consumer_key = 'abcdefghijklmnopqrstuvwxyz'
-  config.consumer_secret = ENV
+  config.consumer_key = ENV'['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
   config.oauth_token = ENV['OAUTH_TOKEN']
   config.oauth_token_secret = ENV['OAUTH_SECRET']
   config.auth_method = :oauth
+end
+
+search("'That fabulous pink hair'") do |tweet|
+  reply "@#{tweet_user(tweet)} That manly dong", tweet
 end
